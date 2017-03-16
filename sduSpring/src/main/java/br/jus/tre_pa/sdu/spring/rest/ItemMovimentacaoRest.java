907e06f0dc205be48f5f0d20546b45112e8e9d2c
jribacruz <jribacruz@gmail.com>
2017-03-16 16:14:06 -0300

    ajuste na listagem de materiais

--- sduSpring/src/main/java/br/jus/tre_pa/sdu/spring/rest/ItemMovimentacaoRest.java
+++ sduSpring/src/main/java/br/jus/tre_pa/sdu/spring/rest/ItemMovimentacaoRest.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import br.jus.tre_pa.sdu.spring.domain.ItemMovimentacao;
 import br.jus.tre_pa.sdu.spring.service.ItemMovimentacaoService;
 
 @RestController
+@CrossOrigin
 public class ItemMovimentacaoRest {
 
 	private Logger log = LoggerFactory.getLogger(ItemMovimentacaoRest.class);
